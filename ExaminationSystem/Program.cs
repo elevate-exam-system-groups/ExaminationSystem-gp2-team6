1bc71ac871adba0814cbefee865c7e2bc3cac66f
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2026-04-28 21:01:44 +0300

    Update Program.cs

--- ExaminationSystem/Program.cs
+++ ExaminationSystem/Program.cs
@@ -12,7 +12,7 @@ namespace ExaminationSystem
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            // Add services to the container..
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
